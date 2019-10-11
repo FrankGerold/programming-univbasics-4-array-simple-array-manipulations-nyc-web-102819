3a8fa593803f60d69b7cb81ef7d1e413019881cd
Frank Gerold <franketsgerold@gmail.com>
2019-10-11 18:38:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,8 +26,8 @@ def using_concat (arr, arr1)
   arr.concat(arr1)
 end
 
-def using_insert (array, element)
-  array.insert(4, element)
+def using_insert (array, element, index = 4)
+  array.insert(index, element)
 end
 
 def using_uniq (arr)
