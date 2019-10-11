ad8b0de90c2e6fc2b0538d6a941a40f7655910e6
Frank Gerold <franketsgerold@gmail.com>
2019-10-11 18:28:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,4 +10,21 @@ def using_pop (array)
   array.pop
 end
 
-def pop_with_args
+def pop_with_args (array, args = 2)
+  array.pop(args)
+end
+
+def using_shift (array)
+  array.shift 
+end
+
+def shift_with_args (arr, arg = 2)
+  arr.shift(arg)
+end
+
+def using_concat (arr, arr1)
+  arr.concat(arr1)
+end
+
+def using_insert (arr, ele, ind = 4)
+  arr.insert 
